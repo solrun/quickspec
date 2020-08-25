b226e73552caa48e03a35b8ced222349681630b3
solrun <solalola@gmail.com>
2020-08-25 11:46:46 +0200

    cleaning up examples

--- template-examples/HugeListsNew.hs
+++ template-examples/HugeListsNew.hs
@@ -4,7 +4,8 @@ import QuickSpec
 import Data.List
 import Control.Monad
 import QuickSpec.Internal.Utils(usort)
-main = qqSpec [
+import Data.Time
+mySig = [
   con "length" (length :: [A] -> Int),
   con "sort" (sort :: [Int] -> [Int]),
   con "scanr" (scanr :: (A -> B -> B) -> B -> [A] -> [B]),
@@ -40,15 +41,22 @@ main = qqSpec [
   con "0" (0 :: Int),
   con "succ" (succ :: Int -> Int)
             ]
-  --,template "id" "?F(X)=X"
-  ,template "fix-point/id" "?F(?X) = ?X"
-  --,template "special constants" "?F(?X) = ?Y" -- This generates too many testcases, takes a long time (hangs for almost 4 minutes)
+  ,template "id" "?F(X)=X"
+  ,template "fixpoint" "?F(?X) = ?X"
+  ,template "cancel" "?F(?G(X)) = ?F(X)"
   ,template "left-id-elem" "?F(?Y,X) = X"
   ,template "right-id-elem" "?F(X,?Y) = X"
-  ,template "cancel" "?F(?G(X)) = ?F(X)"
   ,template "commutative" "?F(X,Y) = ?F(Y,X)"
   ,template "op-commute" "?F(?G(X)) = ?G(?F(X))"
   ,template "2-distributive" "?F(?G(X,Y)) = ?G(?F(X),?F(Y))"
   ,template "analogy-distributive" "?F(?G(X),?G(Y)) = ?G(?H(X,Y))"
   ,template "associative-3" "?F(?F(X,Y),Z) = ?F(X,?F(Y,Z))"
   ]
+main = do
+  start <- getCurrentTime
+  qqSpec mySig
+  qqTime <- getCurrentTime
+--  quickSpec mySig
+--  qsTime <- getCurrentTime
+  print (diffUTCTime qqTime start)
+--  print (diffUTCTime qsTime qqTime)
